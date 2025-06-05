a54227af03bf63012e896ac965d1a2a68c371e19
Tiago Andrade <tiagobbandrade2704@gmail.com>
2025-06-05 17:15:59 -0300

    refactor: add space between products list and categories filter

--- src/modules/products-list/components/products-grid.tsx
+++ src/modules/products-list/components/products-grid.tsx
@@ -38,7 +38,7 @@ export function ProductsGrid() {
   ];
 
   return (
-    <section>
+    <section className="w-full mt-6">
       <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
         {products.map((product) => (
           <li key={product.id}>
